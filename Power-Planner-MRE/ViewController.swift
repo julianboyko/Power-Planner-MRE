990a400397f825b928da0bf7ba6c9564b827db3e
Julian Boyko <julian.boyko@outlook.com>
2020-05-27 14:03:56 -0400

    Initial Commit

---
+++ Power-Planner-MRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Power-Planner-MRE
+//
+//  Created by Julian Boyko on 2020-05-27.
+//  Copyright © 2020 Supreme Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
